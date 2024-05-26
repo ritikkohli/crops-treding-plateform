77e82992457338ea1b7baefd07c03ba8e3393427
ritikkohli <ritikkohli73@gmail.com>
2024-05-26 21:21:44 +0530

    ready to deploy5

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
 import Navbar from "@/components/Navbar";
 
 
@@ -19,10 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <div>
-          {children}
-          <Toaster/>
-        </div>
+        <div>{children}<Toaster/></div>
       </body>
     </html>
   );
