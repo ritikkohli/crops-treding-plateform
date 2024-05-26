709e191870ae7be0be4f10ab0d2090045a2e10cf
ritikkohli <ritikkohli73@gmail.com>
2024-05-26 20:59:00 +0530

    ready to deploy2

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import 'remixicon/fonts/remixicon.css'
 
