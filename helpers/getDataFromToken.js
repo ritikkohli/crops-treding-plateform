c4b62c962e65ac6b8bbdb9b02ab415c3689fea8c
ritikkohli <ritikkohli73@gmail.com>
2024-05-27 17:19:07 +0530

    ok

--- helpers/getDataFromToken.js
+++ helpers/getDataFromToken.js
@@ -1,9 +1,9 @@
 import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken';
 
-export const getDataFromToken = (request) =>{
+export const getDataFromToken = async(request) =>{
     try {
-        const token = request.cookies.get("token").value;
+        const token = await request.cookies.get("token").value;
         const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
         return decodedToken.id;
     } catch (error) {
