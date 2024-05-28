27c8d492f82eca086aabcc8283726f4f7267d427
ritikkohli <ritikkohli73@gmail.com>
2024-05-28 10:41:29 +0530

    ok6

--- app/addcrop/page.jsx
+++ app/addcrop/page.jsx
@@ -1,28 +1,5 @@
-'use client';
-
-import AddCropForm from '@/components/AddCropForm';
-import { useEffect, useState } from 'react';
-
+import AddCropForm from "@/components/AddCropForm"
 
 export default function Addcrop() {
-  const [isFarmer, setIsFarmer]  = useState(false);
-
-
-  const getUserType = async () => {
-    const res = await fetch('api/users/usertype');
-    const {user} = await res.json();
-    const {isFarmer} = user;
-    console.log(isFarmer);
-    setIsFarmer(isFarmer);
-  }
-
-  useEffect(()=>{
-    getUserType()
-  },[])
-
-  if(isFarmer){
       return <AddCropForm/>
-  }else{
-      return <h1>only farmer can access</h1>
-  }
 }
